1cc22ddd4729c5fc7c806bc38d0421cf7c2d7d93
leeharyung <lhr4884@naver.com>
2023-06-14 21:41:30 +0900

    fix: change open

--- module/get_file.py
+++ module/get_file.py
@@ -19,30 +19,28 @@ def get_test_file():
   return test_data_list, label_list
 
 def get_eval_file():
-  file_name = open(Config.eval_file_list, 'r')
-  eval_data_list = []
+  with open(Config.eval_file_list, 'r') as file_name:
+    eval_data_list = []
   
-  for i in file_name.readlines():
-    file = i.strip('\n')
-    
-    with open(Config.eval_file_dir + file + '.raw', 'rb') as f:
-      file = np.fromfile(f, dtype='int16', sep="")
-      eval_data_list.append(file.astype(np.float32))
-  file_name.close()
+    for i in file_name.readlines():
+      file = i.strip('\n')
+      
+      with open(Config.eval_file_dir + file + '.raw', 'rb') as f:
+        file = np.fromfile(f, dtype='int16', sep="")
+        eval_data_list.append(file.astype(np.float32))
   return eval_data_list
 
 
 def get_train_file():
-  file_name = open(Config.train_file_list, 'r')
-  train_data_list = []
-  label_list = []
-  
-  for i in file_name.readlines():
-    i = i.strip('\n')
-    with open(f'raw16k/train/' + i + '.raw', 'rb') as f:
-      file = np.fromfile(f, dtype='int16', sep="")
-      train_data_list.append(file.astype(np.float32))
-      label_list.append(0 if i[0] == 'M' else 1)
-  
-  file_name.close()
+  with open(Config.train_file_list, 'r') as file_name:
+    train_data_list = []
+    label_list = []
+    
+    for i in file_name.readlines():
+      i = i.strip('\n')
+      with open(f'raw16k/train/' + i + '.raw', 'rb') as f:
+        file = np.fromfile(f, dtype='int16', sep="")
+        train_data_list.append(file.astype(np.float32))
+        label_list.append(0 if i[0] == 'M' else 1)
+
   return train_data_list, label_list
