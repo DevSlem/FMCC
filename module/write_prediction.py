0213f40e283b47eb14b558ea1567ee90ba1c6cad
leeharyung <lhr4884@naver.com>
2023-06-14 21:41:14 +0900

    fix: fix newline

--- module/write_prediction.py
+++ module/write_prediction.py
@@ -6,6 +6,6 @@ def write_predictions(predictions, filename):
         for line in file:
             file, _ = line.strip('\n').split(' ')
             test_file_name.append(file)
-    with open(filename, 'w') as file:
+    with open(filename, 'w', newline='\r\n') as file:
         for index, pred in enumerate(predictions):
             file.write((f'{test_file_name[index]} ' + ('feml' if pred == 1 else 'male')) + '\n')
