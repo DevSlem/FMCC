72e11572690ff630594afc71fd01595460b5d384
leeharyung <lhr4884@naver.com>
2023-06-14 21:37:11 +0900

    Revert "fix: change open to with open"
    This reverts commit 36da3abb34e518e64e473a8730a09bce8a7f0bd5.

--- module/get_file.py
+++ module/get_file.py
@@ -4,6 +4,7 @@ from config import Config
 
 def get_test_file():
   with open(Config.test_file_list, 'r') as f:
+    
     test_data_list = []
     label_list = []
 
@@ -18,29 +19,30 @@ def get_test_file():
   return test_data_list, label_list
 
 def get_eval_file():
-  with open(Config.eval_file_list, 'r') as file_name:
-    eval_data_list = []
+  file_name = open(Config.eval_file_list, 'r')
+  eval_data_list = []
   
-    for i in file_name.readlines():
-      file = i.strip('\n')
+  for i in file_name.readlines():
+    file = i.strip('\n')
     
     with open(Config.eval_file_dir + file + '.raw', 'rb') as f:
       file = np.fromfile(f, dtype='int16', sep="")
       eval_data_list.append(file.astype(np.float32))
-
+  file_name.close()
   return eval_data_list
 
 
 def get_train_file():
-  with open(Config.train_file_list, 'r') as file_name:
-    train_data_list = []
-    label_list = []
+  file_name = open(Config.train_file_list, 'r')
+  train_data_list = []
+  label_list = []
   
-    for i in file_name.readlines():
-      i = i.strip('\n')
-      with open(f'raw16k/train/' + i + '.raw', 'rb') as f:
-        file = np.fromfile(f, dtype='int16', sep="")
-        train_data_list.append(file.astype(np.float32))
-        label_list.append(0 if i[0] == 'M' else 1)
+  for i in file_name.readlines():
+    i = i.strip('\n')
+    with open(f'raw16k/train/' + i + '.raw', 'rb') as f:
+      file = np.fromfile(f, dtype='int16', sep="")
+      train_data_list.append(file.astype(np.float32))
+      label_list.append(0 if i[0] == 'M' else 1)
   
+  file_name.close()
   return train_data_list, label_list
--- module/write_prediction.py
+++ module/write_prediction.py
@@ -1,13 +1,11 @@
-from config import Config
-
 def write_predictions(predictions, filename):
-    file_name = Config.test_file_list
+    file_name = f'fmcc_test_ref.txt'
     test_file_name = []
 
     with open(file_name, 'r') as file:
         for line in file:
             file, _ = line.strip('\n').split(' ')
             test_file_name.append(file)
-    with open(filename, 'w', newline='\r\n') as file:
+    with open(filename, 'w') as file:
         for index, pred in enumerate(predictions):
             file.write((f'{test_file_name[index]} ' + ('feml' if pred == 1 else 'male')) + '\n')
