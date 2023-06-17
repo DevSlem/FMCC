079f0e58d561466444f2a2722b017b6f7afdb2da
leeharyung <lhr4884@naver.com>
2023-06-17 23:56:18 +0900

    feat: delete voice file

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ class Config:
   folder_name: str = "preprocessed_data"
   results_dir: str = "results"
   train_file_list: str = 'data/fmcc_train.ctl' 
-  train_file_dir: str = 'data/raw16k/train/'
+  train_file_dir: str = '<YOUR_TRAIN_DATA_DIR>'
   test_file_list: str = 'data/fmcc_test_ref.txt' 
-  test_file_dir: str = 'data/raw16k/test/'
-  eval_file_list: str = 'data/fmcc_test.ctl'
-  eval_file_dir: str = 'data/raw16k/test/'
+  test_file_dir: str = '<YOUR_TRAIN_DATA_DIR>'
+  eval_file_list: str = 'data/fmcc_eval.ctl'
+  eval_file_dir: str = '<YOUR_TRAIN_DATA_DIR>'
